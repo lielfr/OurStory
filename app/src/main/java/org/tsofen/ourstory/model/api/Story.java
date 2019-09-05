fbc2dd0bd95ec5260174a959e886cf2862bdf1fd
egmalik <mmr33e@gmail.com>
2019-09-05 23:47:32 +0300

    impleminting the api of  creating story

---
+++ app/src/main/java/org/tsofen/ourstory/model/api/Story.java
@@ -0,0 +1,114 @@
+package org.tsofen.ourstory.model.api;
+
+import java.io.Serializable;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Story implements Serializable
+{
+
+    @SerializedName("story_id")
+    @Expose
+    private long storyId;
+
+
+    @SerializedName("owner")
+    @Expose
+    private Owner owner;
+    @SerializedName("name_of_person")
+    @Expose
+    private String nameOfPerson;
+    @SerializedName("date_of_birth")
+    @Expose
+    private String dateOfBirth;
+    @SerializedName("date_of_death")
+    @Expose
+    private String dateOfDeath;
+    @SerializedName("picture")
+    @Expose
+    private Object picture;
+    private final static long serialVersionUID = -1652510145870620895L;
+
+    public long getStoryId() {
+        return storyId;
+    }
+
+    public void setStoryId(long storyId) {
+        this.storyId = storyId;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public String getNameOfPerson() {
+        return nameOfPerson;
+    }
+
+    public void setNameOfPerson(String nameOfPerson) {
+        this.nameOfPerson = nameOfPerson;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getDateOfDeath() {
+        return dateOfDeath;
+    }
+
+    public void setDateOfDeath(String dateOfDeath) {
+        this.dateOfDeath = dateOfDeath;
+    }
+
+    public Object getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Object picture) {
+        this.picture = picture;
+    }
+
+
+    public Story(Integer storyId, Owner owner, String nameOfPerson, String dateOfBirth, String dateOfDeath, Object picture) {
+        this.storyId = storyId;
+        this.owner = owner;
+        this.nameOfPerson = nameOfPerson;
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfDeath = dateOfDeath;
+        this.picture = picture;
+    }
+
+    public Story(Owner owner, String nameOfPerson, String dateOfBirth, String dateOfDeath, Object picture) {
+        this.owner = owner;
+        this.nameOfPerson = nameOfPerson;
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfDeath = dateOfDeath;
+        this.picture = picture;
+    }
+
+
+    public Story(Integer storyId, String nameOfPerson, String dateOfDeath, Object picture) {
+        this.storyId = storyId;
+        this.nameOfPerson = nameOfPerson;
+        this.dateOfDeath = dateOfDeath;
+        this.picture = picture;
+    }
+
+    public Story(String nameOfPerson, String dateOfBirth, String dateOfDeath, Object picture) {
+        //this.owner = owner;
+        this.nameOfPerson = nameOfPerson;
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfDeath = dateOfDeath;
+        this.picture = picture;
+    }
+
+}
