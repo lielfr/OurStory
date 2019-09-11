e7d5398e12d2bf14138a272d72b0d5c6fa83324e
Hazar <Hazar.n18@gmail.com>
2019-09-11 15:13:22 +0300

    done a lot of things.

--- app/src/main/java/org/tsofen/ourstory/CommentActivity.java
+++ app/src/main/java/org/tsofen/ourstory/CommentActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -25,7 +26,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class CommentActivity extends AppCompatActivity {
+public class CommentActivity extends Activity {
 
     long userId;
     RecyclerView rv;
@@ -37,6 +38,11 @@ public class CommentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);
         Intent i = getIntent();
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout((int)(width*.8),(int)(height*.8));
         memoryA= (MemoryA) i.getSerializableExtra("memory");
         rv = findViewById(R.id.recycler_comment);
         adapter = new CommentAdapter(memoryA.getComments());
