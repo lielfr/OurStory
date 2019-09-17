46b72f09386e572cd16082d1d4ab707313c64a7b
Liel Fridman <lielft@gmail.com>
2019-09-17 22:04:19 +0300

    Making sure we don't upload when it is a pull request

--- post-success.sh
+++ post-success.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ ( "$TRAVIS_BRANCH" = "master"  ) ]]; then
+if [[ ( "$TRAVIS_BRANCH" = "master"  ) && ( "$TRAVIS_PULL_REQUEST" = "false" ) ]]; then
     cp app/build/outputs/apk/debug/app-debug.apk ./`echo $TRAVIS_COMMIT`-debug.apk
     curl -F file=@`echo $TRAVIS_COMMIT`-debug.apk -F channels="#ourstory-apks" -H "Authorization: Bearer `echo $SLACK_API_TOKEN`" https://slack.com/api/files.upload
 fi
