acaff65a1cdb06acf99a55e7557e61e6659d971b
Liel Fridman <lielft@gmail.com>
2019-09-17 21:53:22 +0300

    Finally, file upload seems to work (at least locally)

--- post-success.sh
+++ post-success.sh
@@ -2,6 +2,5 @@
 
 if [[ ( "$TRAVIS_BRANCH" = "master"  ) ]]; then
     cp app/build/outputs/apk/debug/app-debug.apk ./`echo $TRAVIS_COMMIT`-debug.apk
-    pip3 install -r requirements.txt
-    curl -F file=`echo $TRAVIS_COMMIT`-debug.apk -F channels="#ourstory-apks" -H "Authorization: Bearer $SLACK_API_TOKEN" https://slack.com/api/files.upload
+    curl -F file=@`echo $TRAVIS_COMMIT`-debug.apk -F channels="#ourstory-apks" -H "Authorization: Bearer `echo $SLACK_API_TOKEN`" https://slack.com/api/files.upload
 fi
