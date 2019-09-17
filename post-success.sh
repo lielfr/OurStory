ce4cb7433278865419af4ca02d1b5578f1778b0d
Liel Fridman <lielft@gmail.com>
2019-09-17 21:32:09 +0300

    Maybe this will work

--- post-success.sh
+++ post-success.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ ( "$TRAVIS_BRANCH" = "master"  ) ]]; then
-    cp app/build/outputs/apk/debug/apk-debug.apk ./`echo $TRAVIS_COMMIT`-debug.apk
-    pip install -r requirements.txt
-    python uploader.py `echo $TRAVIS_COMMIT`-debug.apk
+    cp app/build/outputs/apk/debug/app-debug.apk ./`echo $TRAVIS_COMMIT`-debug.apk
+    pip3 install -r requirements.txt
+    python3 uploader.py `echo $TRAVIS_COMMIT`-debug.apk
 fi
