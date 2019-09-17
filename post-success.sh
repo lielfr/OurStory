f9be3ee7e54db73f6b004e9c6a12bae542e87d3d
Liel Fridman <lielft@gmail.com>
2019-09-17 21:43:32 +0300

    We can actually use CURL here

--- post-success.sh
+++ post-success.sh
@@ -3,5 +3,5 @@
 if [[ ( "$TRAVIS_BRANCH" = "master"  ) ]]; then
     cp app/build/outputs/apk/debug/app-debug.apk ./`echo $TRAVIS_COMMIT`-debug.apk
     pip3 install -r requirements.txt
-    python3 uploader.py `echo $TRAVIS_COMMIT`-debug.apk
+    curl -F file=`echo $TRAVIS_COMMIT`-debug.apk -F channels="#ourstory-apks" -H "Authorization: Bearer $SLACK_API_TOKEN" https://slack.com/api/files.upload
 fi
--- uploader.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import os
-import slack
-import sys
-
-client = slack.WebClient(token=os.environ['SLACK_API_TOKEN'])
-response = client.files_upload(
-    channels='#ourstory-apks',
-    file=sys.argv[1])
